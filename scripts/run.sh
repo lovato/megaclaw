bebcd33abc27d6533b02c1c069fe2c77628d6ec6
Marco Lovato <maglovato@gmail.com>
2026-02-24 15:05:09 -0800

    Simplify run.sh to use runtime only, update preconditions to check image

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 MODE="${1:-interactive}"
 
-IMAGE="${2:-$(podman image exists openclaw-runtime && echo openclaw-runtime || echo openclaw-base)}"
-
 if [ "$MODE" = "daemon" ]; then
   podman run -d --rm \
     --name openclaw \
     --network=host \
     -v ./db:/root/.openclaw \
     -v ./logs:/tmp/openclaw \
-    $IMAGE
+    openclaw-runtime
 else
   podman run -it --rm \
     --name openclaw \
     --network=host \
     -v ./db:/root/.openclaw \
     -v ./logs:/tmp/openclaw \
-    $IMAGE
+    openclaw-runtime
 fi
